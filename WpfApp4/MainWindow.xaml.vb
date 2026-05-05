ea79672d2aea5543628275d33fa3c3ae32df0d32
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-05 23:58:21 +0900

    クリアボタンのイベントハンドラを実装

--- WpfApp4/MainWindow.xaml.vb
+++ WpfApp4/MainWindow.xaml.vb
@@ -1,8 +1,24 @@
 ﻿
 Class MainWindow
 
-Private Sub HelloWorldButton_Click(sender As Object, e As RoutedEventArgs)
+Private Sub OnClearButtonClick(sender As Object, e As RoutedEventArgs)
+Dim title As String
+Dim content As String
 
+    title = TitleTextBox.Text
+    content = ContentTextBox.Text
+
+End Sub
+
+Private Sub OnSaveButtonClick(sender As Object, e As RoutedEventArgs)
+Dim result As MessageBoxResult
+
+    result = MessageBox.Show("内容をクリアしますか？", "確認",
+        MessageBoxButton.YesNo, MessageBoxImage.Question)
+    If (result = MessageBoxResult.Yes) Then
+        TitleTextBox.Clear()
+        ContentTextBox.Clear()
+    End If
 End Sub
 
 End Class
