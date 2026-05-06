aefa099a76f8be719c06aee8b08defd75d9d05e4
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-06 23:08:31 +0900

    WPF MVVM アーキテクチャの練習
    Model を実装

---
+++ WpfApp4/CounterModel.vb
@@ -0,0 +1,29 @@
+
+Public Class CounterModel
+
+    Private m_value As Integer
+
+    Public Property Value() As Integer
+        Get
+            Return  Me.m_value
+        End Get
+        Private Set(ByVal value As Integer)
+            Me.m_value = value
+        End Set
+    End Property
+
+    Public Sub Increment()
+        Me.m_value = Me.m_value + 1
+    End Sub
+
+    Public Function CanDecrement() As Boolean
+        CanDecrement = (Me.m_value > 0)
+    End Function
+
+    Public Sub Decrement()
+        If (CanDecrement()) Then
+            Me.m_value = Me.m_value - 1
+        End If
+    End Sub
+
+End Class
