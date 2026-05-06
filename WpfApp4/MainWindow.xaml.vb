198982dd1bc26e5b052d6219092355bc632caa36
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-06 10:19:02 +0900

    ウィンドウデザインの練習
    複雑なデザイン

--- WpfApp4/MainWindow.xaml.vb
+++ WpfApp4/MainWindow.xaml.vb
@@ -1,28 +1,20 @@
 ﻿
 Class MainWindow
 
-Private Sub OnClearButtonClick(sender As Object, e As RoutedEventArgs)
-Dim result As MessageBoxResult
+Private Sub OnAddButtonClick(sender As Object, e As RoutedEventArgs)
+Dim memo As String
+Dim category As String
 
-    result = MessageBox.Show("内容をクリアしますか？", "確認",
-        MessageBoxButton.YesNo, MessageBoxImage.Question)
-    If (result = MessageBoxResult.Yes) Then
-        TitleTextBox.Clear()
-        ContentTextBox.Clear()
+    memo = MemoTextBox.Text
+    category = ""
+    If (CategoryComboBox.SelectedItem IsNot Nothing) Then
+        category = (CategoryComboBox.SelectedItem).Content.ToString()
     End If
-End Sub
 
-Private Sub OnSaveButtonClick(sender As Object, e As RoutedEventArgs)
-Dim title As String
-Dim content As String
-
-    title = TitleTextBox.Text
-    content = ContentTextBox.Text
-
-    MessageBox.Show(
-        "メモを保存しました! (ダミーです)" & vbCrLf & vbCrLf &
-        $"タイトル: {title}{vbCrLf}内容: {content}",
-        "保存完了", MessageBoxButton.OK, MessageBoxImage.Information)
+    If Not String.IsNullOrWhiteSpace(memo) Then
+        MemoListView.Items.Add($"{memo} ({category})")
+        MemoTextBox.Clear()
+    End If
 End Sub
 
 End Class
