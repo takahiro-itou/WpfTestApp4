5ac326677454408eb4a6d7af47b8e8cd0825f1eb
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-06 16:05:01 +0900

    コマンドクラスを新規作成

---
+++ WpfApp4/Counter.vb
@@ -0,0 +1,4 @@
+﻿
+Public Class Counter
+
+End Class
---
+++ WpfApp4/SimpleCommand.vb
@@ -0,0 +1,33 @@
+﻿
+Imports System.Windows.Input
+
+Public Class SimpleCommand
+        Implements ICommand
+
+    Private ReadOnly m_execute As Action(Of Object)
+    Private ReadOnly m_canExecute As Predicate(Of Object)
+
+    Public Sub New(
+            execute As Action(Of Object),
+            Optional canExecute As Predicate(Of Object) = Nothing)
+        Me.m_execute = execute
+        Me.m_canExecute = canExecute
+    End Sub
+
+    Public Function CanExecute(parameter As Object) As Boolean _
+            Implements ICommand.CanExecute
+        Return If(m_canExecute Is Nothing, True, m_canExecute(parameter))
+    End Function
+
+    Public Sub Execute(parameter As Object) Implements ICommand.Execute
+        Me.m_execute(parameter)
+    End Sub
+
+    Public Event CanExecuteChanged As EventHandler _
+            Implements ICommand.CanExecuteChanged
+
+    Public Sub RaiseCanExecuteChanged()
+        RaiseEvent CanExecuteChanged(Me, EventArgs.Empty)
+    End Sub
+
+End Class
