8bb4b6e6cca8f0dc71ae7a46e76b3af47a3983e2
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-07 10:29:18 +0900

    コンポジションルート

--- WpfApp4/Application.xaml.vb
+++ WpfApp4/Application.xaml.vb
@@ -1,7 +1,30 @@
 ﻿
+Imports System.Windows
+
+Imports WpfApp4.Models
+Imports WpfApp4.Services
+Imports WpfApp4.ViewModels
+
 Class Application
 
     ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
     ' can be handled in this file.
+
+Protected Overrides Sub OnStartup(e As StartupEventArgs)
+
+    MyBase.OnStartup(e)
+
+    Dim storage As New JsonCounterStorage()
+
+    Dim model As New CounterModel()
+
+    Dim counterViewModel As New CounterViewModel(model, storage)
+    Dim evenOddViewModel As New EvenOddViewModel(model)
+
+    Dim mainWindow As New MainWindow()
+
+    ' ウィンドウを表示
+    mainWindow.Show()
+End Sub
 
 End Class
