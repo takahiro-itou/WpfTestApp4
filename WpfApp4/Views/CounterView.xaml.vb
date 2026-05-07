595194b0b765d6be5e375ed1794396311cee38e5
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-07 10:54:19 +0900

    CounterView を実装

--- WpfApp4/Views/CounterView.xaml.vb
+++ WpfApp4/Views/CounterView.xaml.vb
@@ -1,7 +1,19 @@
 ﻿
-Namespace Views
+Imports System.Windows.Control
+
+Imports WpfApp4.ViewModels
+
+Namespace WpfApp4.Views
 
 Public Class CounterView
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+    Public Sub SetViewModel(ByVal viewModel As CounterViewModel)
+        Me.DataContext = viewModel
+    End Sub
 
 End Class
 
