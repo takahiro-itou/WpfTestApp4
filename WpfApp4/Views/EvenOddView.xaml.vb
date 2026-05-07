3790cc88f67d0dd1ce563b4b26fcc0d0e44a6b93
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-07 09:56:38 +0900

    Views/EvenOddView を新規作成

---
+++ WpfApp4/Views/EvenOddView.xaml.vb
@@ -0,0 +1,16 @@
+﻿
+Imports System.Windows.Controls
+
+Imports WpfApp4.ViewModels
+
+Public Class EvenOddView
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+    Public Sub SetViewModel(ByVal viewModel As EvenOddViewModel)
+        Me.DataContext = viewModel
+    End Sub
+
+End Class
